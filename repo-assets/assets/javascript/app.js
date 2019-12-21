c3bbf376bc70a134331f7b9547ab48b3f74364ba
Edward Hill <edwardpassanohill@gmail.com>
2019-12-20 19:21:50 -0500

    add gif search functionality

--- repo-assets/assets/javascript/app.js
+++ repo-assets/assets/javascript/app.js
@@ -1,112 +1,32 @@
-// Event listener for our cat-button
-$("#cat-button").on("click", function() {
-  // Storing our giphy API URL for a random cat image
+$("#select-gif").on("click", function(event) {
+  // event.preventDefault() can be used to prevent an event's default behavior.
+  // Here, it prevents the submit button from trying to submit a form when clicked
+  event.preventDefault();
+
+  var gif = $("#gif-input").val();
+
+  // Querying the gify api for the search, the ?app_id parameter is required, but can equal anything
   var queryURL =
-    "https://api.giphy.com/v1/gifs/random?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&tag=cats";
+    "https://api.giphy.com/v1/gifs/random?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&tag=" +
+    gif;
 
   // Perfoming an AJAX GET request to our queryURL
   $.ajax({
     url: queryURL,
     method: "GET"
-  })
+  }).then(function(response) {
+    // Saving the image_original_url property
+    var imageUrl = response.data.image_original_url;
 
-    // After the data from the AJAX request comes back
-    .then(function(response) {
-      // Saving the image_original_url property
-      var imageUrl = response.data.image_original_url;
+    // Creating and storing an image tag
+    var img = $("<img>");
 
-      // Creating and storing an image tag
-      var catImage = $("<img>");
+    // Setting the img src attribute to imageUrl
+    img.attr("src", imageUrl);
+    img.attr("alt", "search image");
 
-      // Setting the catImage src attribute to imageUrl
-      catImage.attr("src", imageUrl);
-      catImage.attr("alt", "cat image");
-
-      // Prepending the catImage to the images div
-      $("#images").prepend(catImage);
-    });
+    // Prepending the img to the images div
+    $("#gif-button-view").prepend(img);
+  });
 });
-
-$("#dog-button").on("click", function() {
-  // Storing our giphy API URL for a random cat image
-  var queryURL =
-    "https://api.giphy.com/v1/gifs/random?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&tag=dogs";
-
-  // Perfoming an AJAX GET request to our queryURL
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  })
-
-    // After the data from the AJAX request comes back
-    .then(function(response) {
-      // Saving the image_original_url property
-      var imageUrl = response.data.image_original_url;
-
-      // Creating and storing an image tag
-      var dogImage = $("<img>");
-
-      // Setting the catImage src attribute to imageUrl
-      dogImage.attr("src", imageUrl);
-      dogImage.attr("alt", "cat image");
-
-      // Prepending the catImage to the images div
-      $("#images").prepend(dogImage);
-    });
-});
-
-$("#fish-button").on("click", function() {
-  // Storing our giphy API URL for a random cat image
-  var queryURL =
-    "https://api.giphy.com/v1/gifs/random?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&tag=fish";
-
-  // Perfoming an AJAX GET request to our queryURL
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  })
-
-    // After the data from the AJAX request comes back
-    .then(function(response) {
-      // Saving the image_original_url property
-      var imageUrl = response.data.image_original_url;
-
-      // Creating and storing an image tag
-      var fishImage = $("<img>");
-
-      // Setting the catImage src attribute to imageUrl
-      fishImage.attr("src", imageUrl);
-      fishImage.attr("alt", "fish image");
-
-      // Prepending the catImage to the images div
-      $("#images").prepend(fishImage);
-    });
-});
-
-$("#fishing-button").on("click", function() {
-  // Storing our giphy API URL for a random cat image
-  var queryURL =
-    "https://api.giphy.com/v1/gifs/random?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&tag=fishing";
-
-  // Perfoming an AJAX GET request to our queryURL
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  })
-
-    // After the data from the AJAX request comes back
-    .then(function(response) {
-      // Saving the image_original_url property
-      var imageUrl = response.data.image_original_url;
-
-      // Creating and storing an image tag
-      var fishingImage = $("<img>");
-
-      // Setting the catImage src attribute to imageUrl
-      fishingImage.attr("src", imageUrl);
-      fishingImage.attr("alt", "fishing image");
-
-      // Prepending the catImage to the images div
-      $("#images").prepend(fishingImage);
-    });
-});
+// Printing the entire object to console
